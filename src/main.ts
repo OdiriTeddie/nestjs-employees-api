63d921cba40316733467c7018603551b815b14c0
Odiri Teddie <odiriteddie.adamu@gmail.com>
2024-04-07 09:41:14 -0700

    nestjs user and employee api with neon db and prisma orm

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './all-exceptions.filter';
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+}
+bootstrap();
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule, {
+//     bufferLogs: true,
+//   });
+//   app.useLogger(app.get(MyLoggerService));
+//   app.enableCors();
+//   app.setGlobalPrefix('api');
+//   await app.listen(3000);
+// }
+// bootstrap();
